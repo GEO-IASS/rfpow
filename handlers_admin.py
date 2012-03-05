9a8edc26ba97bdaaee22641de303ad1bc74def34
g0morrah <g0morrah@092ab664-fcbf-46e8-8362-05d6c20691bb>
2012-03-05 00:30:21 +0000

    Add basic admin panel for Merx parser.
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2012-01/Group_E@34 092ab664-fcbf-46e8-8362-05d6c20691bb

---
+++ handlers_admin.py
@@ -0,0 +1,42 @@
+import webapp2
+import jinja2
+import os
+import logging
+from backend.scheduled import ScheduledParse
+
+class AdminParser(webapp2.RequestHandler):
+
+    def get(self, status=None):
+        self.response.headers['Content-Type'] = 'text/html'
+        jinja_environment = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+        template = jinja_environment.get_template('templates/admin_parsers.html')
+
+        # now stash results into a dict and use it in the top_rfps.html template
+        template_data = {}
+        
+        if status is not None: 
+            template_data['status'] = status
+        self.response.out.write(template.render(template_data))
+
+    def post(self):
+        ignore_duplicates = self.request.get('ignore_duplicates')
+        start_id = self.request.get('start_id')
+        stop_on_dupe = self.request.get('stop_on_dupe')
+        limit = self.request.get('parse_limit')
+
+        (parsed, new) = ScheduledParse.parse_merx( 
+                ignore_duplicates is not '',
+                (start_id is not "") and start_id or None,
+                stop_on_dupe is not '',
+                (limit is not "") and int(limit.strip()) or None )
+
+        self.get( '%d RFPs parsed, %d new. See logs for more information.' % (parsed, new) )
+        
+app = webapp2.WSGIApplication(
+        [('/admin', AdminParser)],
+        debug=True,
+        config= {
+            'webapp2_extras.sessions':
+            { 'secret_key': '6023a964-ea67-4965-b8c1-8b098b87a51a' }
+        })
