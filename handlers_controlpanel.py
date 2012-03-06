bb24c1ac54210b6b63ee9da79bfbe84a9fbd16c0
g0vanish <g0vanish@092ab664-fcbf-46e8-8362-05d6c20691bb>
2012-03-06 02:03:14 +0000

    Fixed handlers_controlpanel.py
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2012-01/Group_E@68 092ab664-fcbf-46e8-8362-05d6c20691bb

--- handlers_controlpanel.py
+++ handlers_controlpanel.py
@@ -111,8 +111,8 @@ class ListKeywordsHandler(BaseHandler):
         jinja_environment = jinja2.Environment(
             loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
         template = jinja_environment.get_template('templates/list_keywords.html')
-	keywords = datastore.query_Keywords(self.request.get('keyword')
-        template_data = {'keywords' : keywords}
+	keywords = datastore.query_Keywords(self.request.get('keyword'))
+        template_data = {'keywords' : keywords }
 
         self.response.out.write(template.render(template_data))
 
