4f9608a7fbcd935c09e45586465a99e130178355
g0morrah <g0morrah@092ab664-fcbf-46e8-8362-05d6c20691bb>
2012-03-19 17:46:39 +0000

    Add template for RFP details.
    * add details button image
    * change stylesheets some moar
    
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2012-01/Group_E@121 092ab664-fcbf-46e8-8362-05d6c20691bb

--- assets/template/js/main.js
+++ assets/template/js/main.js
@@ -2,5 +2,7 @@ $( function() {
     // RFP table behaviour
     var rfp_links = $('.rfp_table .rfp_table_link');
 
-    rfp_links.colorbox( {html: "<h1>Hello, earthlings</h1>"});
+    rfp_links.colorbox( {
+        href: "template_rfp.html"
+    });
 });
