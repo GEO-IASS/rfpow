f58534222fb78aa2b0402aa527c867fe41a4aee1
g0morrah <g0morrah@092ab664-fcbf-46e8-8362-05d6c20691bb>
2012-03-30 16:37:01 +0000

    Add deleting RFPs to admin console. Restyle console.
    git-svn-id: https://stanley.cdf.toronto.edu/svn/csc301-2012-01/Group_E@205 092ab664-fcbf-46e8-8362-05d6c20691bb

--- handlers_admin.py
+++ handlers_admin.py
@@ -4,6 +4,8 @@ import os
 import logging
 import backend.parsers as parsers
 from backend.scheduled import ScheduledParse
+from google.appengine.ext import db
+from backend.models.rfp_entry import *
 from handlers_base import *
 
 class AdminParser(BaseHandler, HTMLRenderer):
@@ -17,29 +19,40 @@ class AdminParser(BaseHandler, HTMLRenderer):
         
         if status is not None: 
             template_data['status'] = status
+
         self.show_rendered_html('templates/admin_parsers.html', template_data)
 
     @user_required
     def post(self):
         """Used to set parser settings and trigger parsers"""
-        parser_name = self.request.get('parser')
-        ignore_duplicates = self.request.get('ignore_duplicates')
-        start_id = self.request.get('start_id')
-        stop_on_dupe = self.request.get('stop_on_dupe')
-        limit = self.request.get('parse_limit')
+        action = self.request.get('action')
 
-        if parser_name == 'merx':
-            parser = parsers.MerxParser()
-        elif parser_name == 'rfp.ca':
-            parser = parsers.RFPParser()
-        elif parser_name == 'satender':
-            parser = parsers.STParser()
-        (parsed, new) = ScheduledParse.parse( 
-                parser,
-                ignore_duplicates is not '',
-                (start_id is not "") and start_id or None,
-                stop_on_dupe is not '',
-                (limit is not "") and int(limit.strip()) or None )
+        # Parse RFPs
+        if action == 'parse':
+            parser_name = self.request.get('parser')
+            ignore_duplicates = self.request.get('ignore_duplicates')
+            start_id = self.request.get('start_id')
+            stop_on_dupe = self.request.get('stop_on_dupe')
+            limit = self.request.get('parse_limit')
 
-        self.get( '%d RFPs parsed, %d new stored. See logs.' % (parsed, new) )
+            if parser_name == 'merx':
+                parser = parsers.MerxParser()
+            elif parser_name == 'rfp.ca':
+                parser = parsers.RFPParser()
+            elif parser_name == 'satender':
+                parser = parsers.STParser()
+            (parsed, new) = ScheduledParse.parse( 
+                    parser,
+                    ignore_duplicates is not '',
+                    (start_id is not "") and start_id or None,
+                    stop_on_dupe is not '',
+                    (limit is not "") and int(limit.strip()) or None )
 
+            self.get( '%d RFPs parsed, %d new stored. See logs.' % (parsed, new) )
+
+        # Delete all RFPs
+        elif action == 'rfp_delete':
+            while len( RFP.all().fetch(1) ) is not 0:
+                db.delete( RFP.all() )
+
+            self.get( 'All RFPs deleted successfully.')
